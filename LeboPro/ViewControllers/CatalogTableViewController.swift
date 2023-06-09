9ab357e8df653b6ea114c200fcfcff92a38af1a2
nasim <nasim_nozirov@mail.ru>
2023-06-09 19:02:52 +0300

    new token

--- LeboPro/ViewControllers/CatalogTableViewController.swift
+++ LeboPro/ViewControllers/CatalogTableViewController.swift
@@ -8,42 +8,93 @@
 import UIKit
 
 class CatalogTableViewController: UITableViewController {
+    private var model = Basket.getProduct()
+    private var viewNavBar = UIView()
 
-    
-    private let model = Basket.getProduct()
-    
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationItem.title = "Каталог"
-        
-        navigationItem.largeTitleDisplayMode = .always
+       private var catalog = CustomLabel(
+        backgroundColor: .white,
+        textColor: .black,
+        title: "Каталог",
+        font: UIFont.systemFont(ofSize: 50)
+       )
        
-    }
+       override func viewDidAppear(_ animated: Bool) {
+           super.viewDidAppear(animated)
+           viewNavBar = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 100))
+           viewNavBar.backgroundColor = .white
+           navigationController?.navigationBar.addSubview(viewNavBar)
+           viewNavBar.addSubview(catalog)
+         fixToConstraint()
+       }
     
-    // MARK: - Table view data source
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return model.product.count
-    }
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           tableView.rowHeight = 145
+           tableView.contentInset = UIEdgeInsets(top: 60, left: 0, bottom: 0, right: 0)
+          
+       }
+       
+       // MARK: - Table view data source
+       override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           
+           return model.product.count
+       }
 
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:  UITableViewCell
-        if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
-            cell = reuseCell
-        } else {
-            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        }
-        
-        var configuration = cell.defaultContentConfiguration()
-        let product = model.product[indexPath.row]
-        configuration.text = product
-        configuration.textProperties.alignment = .natural
-        cell.contentConfiguration = configuration
-        return cell
-    }
+       override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell:  UITableViewCell
+           if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+               cell = reuseCell
+           } else {
+               cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+           }
+           
+           var configuration = cell.defaultContentConfiguration()
+           let product = model.product[indexPath.row]
+           configuration.text = product
+           if let image = UIImage(named: product){
+               configuration.image = image
+           }
+           configuration.textProperties.alignment = .natural
+           configuration.imageProperties.cornerRadius = tableView.rowHeight / 2
+           
+           
+           cell.contentConfiguration = configuration
+           
+           return cell
+       }
+       
+       //УДАЛЯЕМ ЯЧЕЙКИ
+       override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+           let deleteAction = UIContextualAction(style: .destructive, title: "Delete", handler: { _,_,_ in
+               self.model.product.remove(at: indexPath.row)
+               tableView.reloadData()
+           })
+           
+           let editAction = UIContextualAction(style: .normal, title: "Edit", handler: { _,_,_ in
+               
+               tableView.reloadData()
+           })
+           return UISwipeActionsConfiguration(actions: [deleteAction, editAction])
+       }
+       
+       //дополнительная функция удалене итд с левой части экрана появляется
+       override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+           return .none
+       }
+       // чтобы ячейки не сдивигались
+       override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+           return false
+       }
+       
+       private func fixToConstraint() {
+           NSLayoutConstraint.activate([
+             
 
-}
+              catalog.heightAnchor.constraint(equalToConstant: 60),
+              catalog.leadingAnchor.constraint(equalTo: viewNavBar.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+              catalog.bottomAnchor.constraint(equalTo: viewNavBar.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+           ])
+           
+       }
+
+   }
