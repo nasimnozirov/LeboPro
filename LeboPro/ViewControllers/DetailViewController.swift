11121133f4e3fc7cb32790039e3d04c28a358037
nasim <nasim_nozirov@mail.ru>
2023-06-11 20:50:14 +0300

    create ViewCo0ntroler

---
+++ LeboPro/ViewControllers/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  LeboPro
+//
+//  Created by Nasim Nozirov on 11.06.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
